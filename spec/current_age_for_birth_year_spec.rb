35754e2608be32b1a0790ebf9a1c31c60b54a521
zmahfuz817 <zunayed.lm@gmail.com>
2018-02-26 03:27:56 +0000

    automatically backed up by learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -1,5 +1,5 @@
 require_relative '../current_age_for_birth_year.rb'
-def current_age_for_birth_year
+def current_age_for_birth_year(birth_year)
 
 end
 describe "current_age_for_birth_year method" do
